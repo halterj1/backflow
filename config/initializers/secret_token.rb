c72a3662b958aa3c007ac694d6ebb703421cd39e
Jonah <jah132000@gmail.com>
2013-01-18 19:40:39 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Backflow::Application.config.secret_token = '00c3d39779595117560b81c8d29f72ec2a736b52f1e9eae835cac8556554838ce5d963ee52f8275aea267e53bc7771de0a44ebf700798d8dc12750018584b31b'
