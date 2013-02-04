f96a8a9bead8dc0654bd39738afb2163e2af3490
Jonah Halter <jah132000@gmail.com>
2013-02-04 02:26:28 -0600

    Logout for admins

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,12 @@ Backflow::Application.routes.draw do
                :controllers => {
                                  :sessions => 'devise/sessions'},
                :skip => [:sessions] do
-      get '/signin'   => "devise/sessions#new",       :as => :new_user_session
-      post '/signin'  => 'devise/sessions#create',    :as => :user_session
-      get '/signout'  => 'devise/sessions#destroy',   :as => :destroy_user_session
-      get "/signup"   => "devise/registrations#new",  :as => :new_user_registration
-      get "/profile"  => "devise/registrations#edit", :as => :edit_user_registration
+      get     '/signin'   => "devise/sessions#new",       :as => :new_user_session
+      post    '/signin'   => 'devise/sessions#create',    :as => :user_session
+      get     '/signout'  => 'devise/sessions#destroy',   :as => :destroy_user_session
+      get     "/signup"   => "devise/registrations#new",  :as => :new_user_registration
+      get     "/profile"  => "devise/registrations#edit", :as => :edit_user_registration
+      delete  '/signout'  => "devise/sessions#destroy",   :as => :destroy_user_session # Rails Admin Logout
     end
   
   resources :users do
