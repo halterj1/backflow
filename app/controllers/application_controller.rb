2de1c3942c9c69f313eff7dec3b07cea381ad395
Jonah Halter <jah132000@gmail.com>
2013-02-24 02:25:11 -0600

    Updated error 500 access denied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,15 @@ class ApplicationController < ActionController::Base
       params[:user]["next"] || super
     end
   
+  ### This is for Testing ONLY! ###
+  #if Rails.env.development?
+  #  rescue_from Exception, with: :render_500
+  #  rescue_from ActionController::RoutingError, with: :render_404
+  #  rescue_from ActionController::UnknownController, with: :render_404
+  #  rescue_from ActionController::UnknownAction, with: :render_404
+  #  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  #end
+  
   if Rails.env.production?
     unless Rails.application.config.consider_all_requests_local
       rescue_from Exception, with: :render_500
