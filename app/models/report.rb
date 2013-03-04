5a49ae224fee346d0297579034bc43157d16cfe7
Jonah Halter <jah132000@gmail.com>
2013-03-04 00:49:24 -0600

    Fixed report form submission for testers

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,10 +1,4 @@
 class Report < ActiveRecord::Base
-  # attr_accessible :title, :body
-  #devise :database_authenticatable, :registerable,
-  #       :recoverable, :rememberable, :trackable, :validatable
-
-  # Setup accessible (or protected) attributes for your model
-  #attr_accessible :report_ids, :as => :admin
   attr_accessible :customer_name, :customer_number, :account_number, :meter_number, :service_address, :installation_type, :date_test, :assembly_height, :assembly_manufacturer, :assembly_model, :assembly_size, :assembly_number, :supply_pressure, :discharge_pressure, :assembly_type, :remarks
   has_many :comments, as: :commentable
   
